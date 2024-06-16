e819e1963ca7911154daf23e5fd5dc5a3061576f
Aulia Ahmad Gesa <Retiortuk>
2024-06-16 22:10:14 +0700

    Nama Added

--- main.py
+++ main.py
@@ -2,6 +2,15 @@ def khodam_program():
 
    data_khodam = ['Macan Hitam', 'Macan Putih', 'Macan Merah', 'Singa', 'Gajah', 'Ular Kobra', 'Kalajengking', 'Kak Gem', 'Burung Dara', 'Burung Elang']
 
+   print("==== CEK KHODAM ====\n")
+
+   while True:
+      nama = input("Siapa Nama Anda: ")
+      if nama.strip() and nama.isalpha():
+         break
+      else:
+         print("Masukkan Nama Yang Benar")
+
 
 
 khodam_program()
