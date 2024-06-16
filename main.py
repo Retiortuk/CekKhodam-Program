4262a8881da7dcaa2eabeca390e2f074742c83c4
Aulia Ahmad Gesa <Retiortuk>
2024-06-16 21:23:45 +0700

    Data Khodam Added

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("Hello MF")
+def khodam_program():
+
+   data_khodam = ['Macan Hitam', 'Macan Putih', 'Macan Merah', 'Singa', 'Gajah', 'Ular Kobra', 'Kalajengking', 'Kak Gem', 'Burung Dara', 'Burung Elang']
+
+
+
+khodam_program()
